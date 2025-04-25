584a28ebfa5e97c2e2384e7e003c37465d2e3409
dselva-2005 <dselvajagan@gmail.com>
2025-04-25 20:47:28 +0530

    launch into interet

--- Core/settings.py
+++ Core/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x2dut5ag(komcd6cex46m32l9lwa2bor8n%wj$3w5q^@h-06!d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
-# DEBUG = False
+DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1','igniteandascend.com']
+ALLOWED_HOSTS = ['127.0.0.1','igniteandascend.com']
 
 
 # Application definition
